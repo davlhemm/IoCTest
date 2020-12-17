5c225b94b547030e560d55c1704ad1df2db58bca
David Hemmenway <david.hemmenway@rig-rds.com>
2020-12-17 02:01:26 -0600

    File import test

--- IoCTest/Model/Logger.cs
+++ IoCTest/Model/Logger.cs
@@ -4,26 +4,66 @@ using System.IO;
 
 namespace IoCTest.Model 
 { 
-/// <summary>
-/// Requires a writer, decoupled but not required by interface...
-/// </summary>
-public abstract class Logger : ILogger
-{
-    private readonly TextWriter _writer;
-
-    protected Logger() { throw new NotImplementedException("Can't create logger here..."); }
-
-    protected Logger(TextWriter writer)
+    /// <summary>
+    /// Requires a writer, decoupled but not required by interface...
+    /// </summary>
+    public abstract class Logger : ILogger
     {
-        _writer = writer;
+        private readonly TextWriter _writer;
+
+        protected Logger() { throw new NotImplementedException("Can't create logger here..."); }
+
+        protected Logger(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public virtual void Log(string message)
+        {
+            //Don't handle writer disposal here...
+            _writer.WriteLine(message);
+            _writer.Flush();
+            //_writer.Close();
+        }
     }
 
-    public virtual void Log(string message)
+    /// <summary>
+    /// Concrete logger implementation for testing
+    /// </summary>
+    public class FileLogger : Logger
     {
-        //Don't handle writer disposal here...
-        _writer.WriteLine(message);
-        _writer.Flush();
-        //_writer.Close();
+        private readonly string _logFile;
+
+        protected FileLogger()
+        {
+            throw new NotImplementedException("Can't create logger here...");
+        }
+
+        /// <summary>
+        /// My documents default location for file.
+        /// </summary>
+        /// <param name="logFile"></param>
+        public FileLogger(string logFile)
+            : base(new StreamWriter
+            (Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                logFile)))
+        {
+            _logFile = logFile;
+        }
+
+        public FileLogger(string logFile, Environment.SpecialFolder specialFolder)
+            : base(new StreamWriter
+            (Path.Combine(Environment.GetFolderPath(specialFolder),
+                logFile)))
+        {
+            _logFile = logFile;
+        }
+
+        public FileLogger(string logFile, TextWriter writer)
+            : base(writer)
+        {
+            _logFile = logFile;
+        }
     }
-}
+
 }
---
+++ IoCTest/Processes/FileImportProcess.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace IoCTest.Processes
+{
+    public class FileImportProcess
+    {
+        private readonly string _baseImportPath;
+        private readonly string _extension;
+        private readonly SearchOption _searchOption;
+        
+        private FileImporter Importer { get; set; }
+
+        private FileImportProcess() { }
+        
+        //TODO: Configurable base path
+        public FileImportProcess(string baseImportPath, string extension)
+        {
+            _baseImportPath = baseImportPath;
+            _extension = extension;
+            _searchOption = SearchOption.TopDirectoryOnly;
+        }
+
+        public FileImportProcess(string baseImportPath, string extension, SearchOption searchOption)
+        {
+            _baseImportPath = baseImportPath;
+            _extension = extension;
+            _searchOption = searchOption;
+        }
+
+        /// <summary>
+        /// Import files from base path with wildcard extension
+        /// </summary>
+        /// <returns></returns>
+        public IList<string> GetImportFiles()
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(_baseImportPath, "*"+_extension);
+            return files.ToList();
+        }
+    }
+
+    public class FileImporter : IFileImporter
+    {       
+        public void Import()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IFileImporter
+    {
+        public void Import();
+    }
+}
