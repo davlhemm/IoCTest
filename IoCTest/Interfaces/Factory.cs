516cf001c985ec496d0db964a782fd13348c006f
David Hemmenway <david.hemmenway@rig-rds.com>
2021-02-01 20:31:33 -0600

    Compression lib separation, backup service cleanup IoC

--- IoCTest/Interfaces/Factory.cs
+++ IoCTest/Interfaces/Factory.cs
@@ -32,47 +32,51 @@ namespace IoCTest.Interfaces
      * Type-sensitive at runtime for interpreting
      */
     
-
+    /// <summary>
+    /// This the progenitor of different typed items...
+    /// <para>See what options exist for type-ambiguity instead of declarative</para>
+    /// </summary>
     public enum DataItemVariation
     {
+        Default,
         First,
         Second,
-        Third,
-        Default
+        Third
     }
 
 
-    public class DataItemFactory<T> 
-        where T : new()
+    public class DataItemFactory
     {
         /// <summary>
         /// Generate a data item based on declarative enum
         /// </summary>
         /// <param name="enumQualifier"></param>
         /// <returns></returns>
-        public static IDataItem<T> CreateDataItem(DataItemVariation enumQualifier)
+        public static IDataItem<T> CreateDataItem<T>(DataItemVariation enumQualifier)
+        where T: new()
         {
-            switch (enumQualifier)
-            {
-                case DataItemVariation.First:
-                    return new DataItem<T>();
-                default:
-                    return new DataItem<T>();
-            }
+            T tItem = new T();
+            return new DataItem<T>(tItem, enumQualifier);
         }
     }
 
     public class DataItem<T> : IDataItem<T>
     {
-        public T Item => throw new NotImplementedException();
+        public DataItem(T item, DataItemVariation dataEnum)
+        {
+            Item = item;
+            DataItemEnum = dataEnum;
+        }
 
-        public int EnumAssociatedWithItemCreation => throw new NotImplementedException();
+        public T Item { get; set; }
+        
+        public DataItemVariation DataItemEnum { get; set; }
     }
 
     public interface IDataItem<out T>
     {
         T Item { get; }
-        int EnumAssociatedWithItemCreation { get; }
+        DataItemVariation DataItemEnum { get; }
     }
 
     public interface IEnumFactory
---
+++ IoCTest/Processes/FileImport.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using IoCTest.Interfaces;
+
+namespace IoCTest.Processes
+{
+    /// <summary>
+    /// <para>File Import Process should be what's related to individual file imports...</para>
+    /// <remarks>NOTE: Replace with a builder/factory for file importers</remarks>
+    /// </summary>
+    public class FileImport : IFileImport
+    {
+        private readonly IFileImportInfo _importInfo;
+        public IFileImportInfo ImportInfo => _importInfo;
+
+
+        private FileImport() { }
+        
+        //TODO: Configurable base path
+        public FileImport(IFileImportInfo importInfo)
+        {
+            _importInfo = importInfo;
+        }
+        
+
+        /// <summary>
+        /// Import files from base path with wildcard extension
+        /// </summary>
+        /// <returns></returns>
+        public IList<string> GetImportFiles()
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(_importInfo.BaseImportPath, _importInfo.SearchPattern, _importInfo.SearchOption);
+            return files.ToList();
+        }
+    }
+
+    public interface IFileImport
+    {
+        IFileImportInfo ImportInfo { get; }
+        IList<string> GetImportFiles();
+    }
+
+    public class FileImportInfo : IFileImportInfo
+    {
+        public string BaseImportPath { get; }
+        public string SearchPattern { get; }
+        public SearchOption SearchOption { get; } = SearchOption.TopDirectoryOnly;
+
+        public FileImportInfo()
+        {
+            BaseImportPath = "C:\\";
+            SearchPattern = "*";
+        }
+
+        public FileImportInfo(string baseImportPath, string searchPattern)
+        {
+            BaseImportPath = baseImportPath;
+            SearchPattern = searchPattern;
+        }
+    }
+
+    public interface IFileImportInfo
+    {
+        string BaseImportPath { get; }
+        string SearchPattern { get; }
+        SearchOption SearchOption { get; }
+    }
+}
