b5ec3d5961f8bc05b78c7054e889a7d1154071a3
David Hemmenway <David.Hemmenway@rig-rds.com>
2021-02-02 11:49:26 -0600

    Fast Forward from master

--- IoCTest/Interfaces/Backup.cs
+++ IoCTest/Interfaces/Backup.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using ICSharpCode.SharpZipLib.Zip;
+using Compression;
 
 namespace IoCTest.Interfaces
 {
-    public class BackupService : IBackup, IDisposable
+    public class BackupService : IDisposable
     {
         private readonly IBackup _backupStrategy;
+        public IBackup BackupStrategy => _backupStrategy;
 
         private BackupService()
         {
@@ -31,27 +32,14 @@ namespace IoCTest.Interfaces
         
         public void Dispose() { }
 
-        public void MakeBackup(IList<string> listOfFiles, string outDirectory, string outFileName)
-        {
-            _backupStrategy.MakeBackup(listOfFiles, outDirectory, outFileName);
-        }
-
-        public void MakeBackup(string inDirectory, string outDirectory, string outFileName)
-        {
-            _backupStrategy.MakeBackup(inDirectory, outDirectory, outFileName);
-        }
-
-        //public string GetDirectory(string whereTo)
+        //public void MakeBackup(IList<string> listOfFiles, string outDirectory, string outFileName)
         //{
-        //    FolderBrowserDialog dialog = new FolderBrowserDialog { Description = whereTo };
-        //    DialogResult result = dialog.ShowDialog();
-
-        //    return GetDirectory(dialog.SelectedPath, result);
+        //    _backupStrategy.MakeBackup(listOfFiles, outDirectory, outFileName);
         //}
 
-        //public string GetDirectory(string selectedPath, DialogResult result)
+        //public void MakeBackup(string inDirectory, string outDirectory, string outFileName)
         //{
-        //    return result == DialogResult.OK ? selectedPath : string.Empty;
+        //    _backupStrategy.MakeBackup(inDirectory, outDirectory, outFileName);
         //}
     }
 
@@ -60,7 +48,7 @@ namespace IoCTest.Interfaces
         public void MakeBackup(IList<string> listOfFiles, string outDirectory, string outFileName)
         {
             //Journey into dependent code...
-            CompressDirectory(listOfFiles, outDirectory, outFileName);
+            Compression.Compression.CompressDirectory(listOfFiles, outDirectory, outFileName);
         }
 
 #if !NET5_0
@@ -70,78 +58,6 @@ namespace IoCTest.Interfaces
             MakeBackup(dirInfo.GetFiles().Select(x => x.FullName).ToList(), outDirectory, outFileName);
         }
 #endif
-
-        /// <summary>
-        /// Method that compress all the files inside a folder (non-recursive) into a zip file.
-        /// </summary>
-        /// <param name="files"></param>
-        /// <param name="outDirectory"></param>
-        /// <param name="outFileName">Used as explicit name for strategies that use single file and an extension for those that do per-file.</param>
-        /// <param name="compressionLevel"></param>
-        private static void CompressDirectory(IList<string> files, string outDirectory, string outFileName, int compressionLevel = 9)
-        {
-            try
-            {
-                // 'using' statements guarantee the stream is closed properly which is a big source
-                // of problems otherwise. Exception safe.
-
-                //Define output stream based on full-path filename
-                using (ZipOutputStream outputStream = new ZipOutputStream(File.Create(outDirectory + "\\" + outFileName))) //zipName)))
-                {
-                    // Define the compression level
-                    // 0 - store only to 9 - means best compression
-                    outputStream.SetLevel(compressionLevel);
-
-                    byte[] buffer = new byte[4096];
-
-                    foreach (var file in files)
-                    {
-                        // Using GetFileName makes the result compatible with XP
-                        // as the resulting path is not absolute.
-                        ZipEntry entry = new ZipEntry(Path.GetFileName(file));
-
-                        // Setup the entry data as required.
-
-                        // Crc and size are handled by the library for seekable streams
-                        // so no need to do them here.
-
-                        // Could also use the last write time or similar for the file.
-                        entry.DateTime = DateTime.Now;
-                        outputStream.PutNextEntry(entry);
-
-                        using (FileStream fs = File.OpenRead(file))
-                        {
-
-                            // Using a fixed size buffer here makes no noticeable difference for output
-                            // but keeps a lid on memory usage.
-                            int sourceBytes;
-
-                            do
-                            {
-                                sourceBytes = fs.Read(buffer, 0, buffer.Length);
-                                outputStream.Write(buffer, 0, sourceBytes);
-                            } while (sourceBytes > 0);
-                        }
-                    }
-
-                    // Finish/Close aren't needed strictly as the using statement does this automatically
-
-                    // Finish is important to ensure trailing information for a Zip file is appended.  Without this
-                    // the created file would be invalid.
-                    outputStream.Finish();
-
-                    // Close is important to wrap things up and unlock the file.
-                    outputStream.Close();
-
-                    Console.WriteLine("Files successfully compressed");
-                }
-            }
-            catch (Exception ex)
-            {
-                // No need to rethrow the exception as for our purposes its handled.
-                Console.WriteLine("Exception during processing {0}", ex);
-            }
-        }
     }
 
     /// <summary>
